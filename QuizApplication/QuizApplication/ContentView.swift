532487933158c1fa8248440dbef9fe9402160137
Darshan <>
2023-06-16 10:41:37 +0530

    New commit

---
+++ QuizApplication/QuizApplication/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuizApplication
+//
+//  Created by IPS-161 on 16/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+        
+        
+        //New
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuizApplication/QuizApplication/QuizApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApplicationApp.swift
+//  QuizApplication
+//
+//  Created by IPS-161 on 16/06/23.
+//
+
+import SwiftUI
+
+@main
+struct QuizApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
